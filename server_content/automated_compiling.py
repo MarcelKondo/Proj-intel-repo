65242bfe2f249bdd4d6ef124e3db04fd8f57de9c
MinhaTu <ailson.alexandre@hotmail.com>
2021-03-07 22:03:09 +0100

    corrected 1 automated_compiling

--- server_content/automated_compiling.py
+++ server_content/automated_compiling.py
@@ -6,13 +6,15 @@ def define_copiler_settings(opLevel, simdType,n1 = 256 ,n2 = 256 ,n3 = 256 , nb_
                             tblock1 = 32 ,tblock2 = 32, tblock3 = 32,):
     os.system("cd ~/Proj-Intel/Appli-iso3dfd/")
     os.system("make -e OPTIMIZATION=\"-O"+ str(opLevel) + "\" -e simd=" + simdType + " last" )
+    #make -e OPTIMIZATION="-O3" -e simd=sse last
+
     os.system("cd ~/Proj-Intel/Appli-iso3dfd/bin")
 
-    res = subprocess.run("iso3dfd_dev05_cpu_" + "simdType" + ".exe", shell=True,
+    res = subprocess.run("iso3dfd_dev05_cpu_" + simdType + ".exe", shell=True,
                          stdout=subprocess.PIPE)
     print(res.stdout)
     return res.stdout
 
-print("starting execution")
+print("V2 starting execution")
 define_copiler_settings(opLevel = 3, simdType = "sse")
 print("execution finished")
