23f7327f6626a01911856e4aeb58dd68f23ef504
MinhaTu <ailson.alexandre@hotmail.com>
2021-03-07 22:07:37 +0100

    corrected 3 automated_compiling.py

--- server_content/automated_compiling.py
+++ server_content/automated_compiling.py
@@ -4,14 +4,14 @@ import subprocess
 
 def define_copiler_settings(opLevel, simdType,n1 = 256 ,n2 = 256 ,n3 = 256 , nb_threads = 4, nb_it = 100 , 
                             tblock1 = 32 ,tblock2 = 32, tblock3 = 32,):
-    os.system("cd ~/Proj-Intel/Appli-iso3dfd/")
-    os.system("pwd")
-    os.system("make -e OPTIMIZATION=\"-O"+ str(opLevel) + "\" -e simd=" + simdType + " last" )
+    #os.system("cd ~/Proj-Intel/Appli-iso3dfd/")
+    #os.system("pwd")
+    os.system("cd ~/Proj-Intel/Appli-iso3dfd/ && make -e OPTIMIZATION=\"-O"+ str(opLevel) + "\" -e simd=" + simdType + " last" )
     #make -e OPTIMIZATION="-O3" -e simd=sse last
 
     os.system("cd ~/Proj-Intel/Appli-iso3dfd/bin")
     os.system("pwd")
-    res = subprocess.run("iso3dfd_dev05_cpu_" + simdType + ".exe", shell=True,
+    res = subprocess.run("cd ~/Proj-Intel/Appli-iso3dfd/bin && iso3dfd_dev05_cpu_" + simdType + ".exe", shell=True,
                          stdout=subprocess.PIPE)
     print(res.stdout)
     return res.stdout
