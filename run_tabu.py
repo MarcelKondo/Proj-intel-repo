d8cd937ccf1a2b3e4015462f232f02baa82cfce5
xamirb <80265845+xamirb@users.noreply.github.com>
2021-03-10 15:22:41 +0100

    add run_tabu.py

---
+++ run_tabu.py
@@ -0,0 +1,18 @@
+import tabu_greedy
+
+S0 = {
+    'n1' : 256,
+    'n2' : 256,
+    'n3' : 256,
+    'nb_threads' : 4,
+    'nb_it' : 100,
+    'tblock1' : 32,
+    'tblock2' : 32,
+    'tblock3' : 32,
+    'simdType' : "sse"
+}
+IterMax = 5
+tabu_size = 10
+eb, Sb, iters = tabu_greedy.tabu_greedy(S0,IterMax,tabu_size)
+
+print(f"Best score: {eb}, Solution: {str(Sb)}, Iters: {iters}")
