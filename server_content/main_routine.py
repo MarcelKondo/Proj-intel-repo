846525612a8bbbaffb866b49348c3be5b308ff78
MinhaTu <ailson.alexandre@hotmail.com>
2021-03-14 23:37:54 +0100

    fixed bugs in CLI args

--- server_content/main_routine.py
+++ server_content/main_routine.py
@@ -4,9 +4,11 @@ import sys
 
 S0 = [240, 240, 240, 3, 100, 32, 32, 32]
 
-nb_args = len(sys.argv) - 1
+nb_args = len(sys.argv) - 1 #first element is the script name
+args = sys.argv[1:]
+args = [int(i) for i in args]
 if(nb_args != 0):
-    S0[0:nb_args] = sys.argv
+    S0[0:nb_args] = args
 res = HC.HillClimbing(S0, 20, [0, 1, 2], "flops")
 print(res)
 print("Finished")
