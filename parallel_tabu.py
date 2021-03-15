85224aeda481c97e9c557abf6434ed19d01eb053
xamirb <80265845+xamirb@users.noreply.github.com>
2021-03-15 15:43:54 +0100

    Update parallel_tabu.py

--- parallel_tabu.py
+++ parallel_tabu.py
@@ -117,7 +117,7 @@ def parallel_tabu_greedy(S0,IterMax,tabu_size, NbP, Me):
             eb = e
             L_tabu = fifo_add(Sb, L_tabu, tabu_size)
             LNgbh = get_neighbourhood(Sb)
-            pint(LNgbh)
+            print(LNgbh)
         else:
             NewBetterS = False
         iter += 1
