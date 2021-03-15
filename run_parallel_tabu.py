bc823f77efe98f4e800674546efa91e5b48ca8b4
xamirb <80265845+xamirb@users.noreply.github.com>
2021-03-15 14:54:18 +0100

    Create run_parallel_tabu.py

---
+++ run_parallel_tabu.py
@@ -0,0 +1,20 @@
+import parallel_tabu
+from server_content.automated_compiling import define_copiler_settings
+
+define_copiler_settings(opLevel=3, simdType="avx512")
+
+S0 = {
+    'n1' : 256,
+    'n2' : 256,
+    'n3' : 256,
+    'nb_threads' : 4,
+    'nb_it' : 10,
+    'tblock1' : 32,
+    'tblock2' : 32,
+    'tblock3' : 32,
+    'simdType' : "avx512"
+}
+IterMax = 5
+tabu_size = 2
+eb, Sb, iters = parallel_tabu.parallel_tabu(S0,IterMax,tabu_size, NbP, Me)
+print(f"Best score: {eb}, Solution: {str(Sb)}, Iters: {iters}")
