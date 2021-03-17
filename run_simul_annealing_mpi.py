2c037907b7a9def2f49cfab11f2d47359e545c26
Marcel Kondo <mkkondo@gmail.com>
2021-03-17 16:01:48 +0100

    update mpi annealing

--- run_simul_annealing_mpi.py
+++ run_simul_annealing_mpi.py
@@ -50,10 +50,8 @@ else :
 Eb = np.array([eb],dtype=np.float64)
 comm.Gather(Eb,EbTab,root=0)
 
-if Me == 0:
-  print(np.array(Sb.values()))
-comm.Gather(np.array(Sb.values()),SbTab,root=0)
-comm.Gather(np.array(S0.values()),S0Tab,root=0)
+comm.Gather(np.array([x for x in Sb.values()]),SbTab,root=0)
+comm.Gather(np.array([x for x in S0.values()]),S0Tab,root=0)
 
 Iter = np.array([iter],dtype=int)
 comm.Gather(Iter,IterTab,root=0)
