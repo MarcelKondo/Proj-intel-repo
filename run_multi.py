b6a4a594694203eb7ac37a24e9b5b32c45a30448
charles217 <66557974+charles217@users.noreply.github.com>
2021-03-17 16:11:07 +0100

    Update run_multi.py

--- run_multi.py
+++ run_multi.py
@@ -1,6 +1,6 @@
 from mpi4py import MPI
 import os
-import deploy_greedy_v2
+import deploy_greedy_v3
 import parallel_tabu
 #import mpi_HillClimbing
 #import run_simul_annealing_mpi
@@ -38,8 +38,6 @@ def parse():
 if __name__ == "__main__":
     #print('ARGS', sys.argv[1:])
     args = parse()
-    
-    
     #run hill climbing
     print(20*"=","HILL CLIMBING",20*"=")
     #ebhill, Sbhill,itershill= mpi_HillClimbing.HillClimbing(S0,args.iter_max,param_list,cost_type):
@@ -49,7 +47,7 @@ if __name__ == "__main__":
     
     #run greedy
     print(20*"=","GREEDY",20*"=")
-    eb, Sb, iters = deploy_greedy_v2.parallel_greedy(S0,args.iter_max, NbP, Me)
+    eb, Sb, iters = deploy_greedy_v3.parallel_greedy(S0,args.iter_max, NbP, Me)
     print(f"Best score: {eb}, Solution: {str(Sb)}, Iters: {iters}")
     
     #run tabu_greedy
@@ -60,15 +58,19 @@ if __name__ == "__main__":
     print(f"Best score: {ebtab}, Solution: {str(Sbtab)}, Iters: {iterstab}")
 
     #run annealing
-    print(20*"=","ANNEALING",20*"=")
-    cmd = 'mpirun -np 4 -map-by ppr:2:socket -bind-to socket python3 run_simul_annealing_mpi.py'
-    os.system(cmd)
+    #print(20*"=","ANNEALING",20*"=")
+    #cmd = 'mpirun -np 4 -map-by ppr:2:socket -bind-to socket python3 run_simul_annealing_mpi.py'
+    #os.system(cmd)
     
     
     Lmethod= [[eb, Sb, iters],[ebtab, Sbtab, iterstab]]
     if eb>ebtab:
-        print("greedy simple is better")
+        print("Greedy simple is better")
         print("eb",eb,"Sb",Sb,"iters", iters)
+        print("greedy tabu result")
+        print("ebtab",ebtab,"Sbtab",Sbtab,"iterstab", iterstab)
     else:
         print("greedy tabu is better")
         print("ebtab",ebtab,"Sbtab",Sbtab,"iterstab", iterstab)
+        print("Greedy simple result")
+        print("eb",eb,"Sb",Sb,"iters", iters)
