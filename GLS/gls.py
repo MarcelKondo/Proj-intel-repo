8f99a7b27a301c0846f517955aa99740cfd12dbe
xamirb <80265845+xamirb@users.noreply.github.com>
2021-03-22 17:58:04 +0100

    Update gls.py

--- GLS/gls.py
+++ GLS/gls.py
@@ -1,2 +1,17 @@
-print('Hello')
+def ChoosePenaltyFeatures(p,c):
+  s = len(p)*[0]
+  for i in range(len(p)):
+    s[i] = c[i]/(p[i]+1)
+  index_max = s.index(max(s))
+  p[index_max]+=1
+  return p
 
+
+def ComputeC(S,Sb,keys):
+  for key in keys: 
+  c = ATERMINER
+  
+    
+    
+    
+    
