720ca5748ba2549f47dfe579b912df80cbfece8d
charles217 <66557974+charles217@users.noreply.github.com>
2021-03-22 09:53:07 +0100

    Update run_parallel_tabu.py

--- run_parallel_tabu.py
+++ run_parallel_tabu.py
@@ -33,8 +33,9 @@ def parse():
 
 if __name__ == "__main__":
     #print('ARGS', sys.argv[1:])
+    gamma= 1
     args = parse()
-    eb, Sb, iters = parallel_tabu.parallel_tabu_greedy(S0,args.iter_max,args.tabu_size, NbP, Me)
+    eb, Sb, iters = parallel_tabu.parallel_tabu_greedy(S0,args.iter_max,args.tabu_size, NbP, Me,gamma)
     print(f"Best score: {eb}, Solution: {str(Sb)}, Iters: {iters}")
 
 
