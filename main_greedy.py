80c5a45fb894eb8704c00f65a53be0db95510a7d
MinhaTu <ailson.alexandre@hotmail.com>
2021-03-26 23:00:28 +0100

    S0.pop simdType when printing

--- main_greedy.py
+++ main_greedy.py
@@ -43,10 +43,12 @@ def execute(S0, args):
 
 
     S0.pop('simdType',None) #simdType not int and useless at this point
-    
+
     S0_a = np.fromiter(S0.values(), dtype = int)
     comm.Gather(S0_a,S0Tab,root=0)
     
+    GR_sb.pop('simdType',None) #simdType not int and useless at this point
+
     GR_sb_a = np.fromiter(GR_sb.values(), dtype = int)
     comm.Gather(GR_sb_a,SbTab,root=0)
 
