95e557e88e40ed606de3f3009adfa0a277482134
charles217 <66557974+charles217@users.noreply.github.com>
2021-04-02 11:53:10 +0200

    Update guided_search_v2.py

--- guided_search_v2.py
+++ guided_search_v2.py
@@ -95,7 +95,7 @@ def parallel_greedy(S0,IterMax,NbP, Me,penalties,c, listparam,lba):
    
     Sb = S0
     #print("so",S0)
-    eb= Cost(Sb)
+    #eb= Cost(Sb)
     eb = fcost(Sb,penalties, Sb, eb,listparam,lba)
     print("fcost dans parall greedy")
     iter = 0
@@ -145,10 +145,8 @@ def Guided(S0,IterMax,NbP, Me,IterMaxG):
                  e = eb
                  #fcost = 
                  while iterG < IterMaxG and NewBetterSG:
-                      print("je suis arrivé dans la boucle")
-                      fcostg = fcost(S,penalties, Sb, eb,listparam,lba)
-                      print("fcost",fcostg)
-                      c=ComputeC(S,fcostg,Sb,eb,listparam) #Cost(Sb)## pb ici S pas assigné avant ComputeC(S,Sb) un fcost n'est pas calculé avan
+                      
+                      c=ComputeC(S,Sb,eb,listparam) #Cost(Sb)## pb ici S pas assigné avant ComputeC(S,Sb) un fcost n'est pas calculé avan
                       penalties= ChoosePenaltyFeatures(penalties,c)
                       
                       e,S,iter = parallel_greedy(Sb,IterMax,NbP, Me,penalties,c,listparam,lba)
