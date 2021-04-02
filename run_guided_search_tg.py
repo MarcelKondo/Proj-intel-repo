eef8705c405a8fe29483be754656e51cf19be12c
charles217 <66557974+charles217@users.noreply.github.com>
2021-04-02 15:27:59 +0200

    Create run_guided_search_tg.py

---
+++ run_guided_search_tg.py
@@ -0,0 +1,46 @@
+Guided(S0,IterMax,tabu_size,NbP, Me,IterMaxG):
+from mpi4py import MPI
+import guided_search_tg
+import sys, getopt, argparse
+import general_config as GC
+from server_content.automated_compiling_tabu import define_copiler_settings
+
+
+define_copiler_settings(opLevel=3, simdType="avx512")
+comm = MPI.COMM_WORLD
+NbP = comm.Get_size()
+Me = comm.Get_rank()
+
+S0 = GC.generateS0()
+
+
+param_space = {
+    'n1' : [256, 1024, 16],
+    'n2' : [256, 1024, 16],
+    'n3' : [256, 1024, 16],
+    'nb_threads' : [4, 10, 0],
+    'nb_it' : [10, 20, 0],
+    'tblock1' : [32, 128, 16],
+    'tblock2' : [32, 128, 4],
+    'tblock3' : [32, 128, 4],
+    'simdType' : ["avx512"]
+}
+
+
+def parse():
+    parser = argparse.ArgumentParser('Greedy Parallel Tabu')
+    parser.add_argument('-itm', '--iter_max', type=int, metavar='',required=True,help='IterMax')
+    parser.add_argument('-itmG', '--iter_maxG', type=int, metavar='',required=True,help='IterMaxG')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    #print('ARGS', sys.argv[1:])
+    args = parse()
+    eb, Sb, iters, penalties, c, iterG= guided_search_tg.Guided(S0,args.iter_max, NbP, Me,args.iter_maxG)
+    print(f"Best score: {eb}, Solution: {str(Sb)}, Iters_last_greedy: {iters},IterGuided: {iterG},Penalties: {penalties},c: {c}")
+    
+    
+    
+    
