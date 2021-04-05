cda33da1340511b66545a0d1c0aaedeea0a35076
MinhaTu <ailson.alexandre@hotmail.com>
2021-04-05 22:11:36 +0200

    implemented graph

--- function_local_methods.py
+++ function_local_methods.py
@@ -78,6 +78,7 @@ def execute(args):
         if(Me == 0):
             t1 = time.time()
             print(20*"=","HILL CLIMBING",20*"=")
+            print(f"DEBUUUUUUUG INITIAL SOLUTION {S0}")
             best_E, best_Sb, iters_HC = HC.HillClimbing(S0, args.iter_max, "flops")
             best_S0 = S0
             dt = time.time()-t1
@@ -151,103 +152,7 @@ def execute(args):
             print("eb_HC",best_E,"Sb_HC",best_Sb,"iters_HC", iters_SA)
             print("Execution time {:.3f}".format(dt))
             print('\n')
-        #best_E, best_S0, best_Sb = main_SA.execute(S0, args)
-        # print("eb_HC",eb_HC,"Sb_HC",Sb_HC,"iters_HC", iters_HC)
-        # print('\n')
-        # print("\n")
-        # print("========================= Best Parameters ======================")
-        # print("Simulated Annealing")
-        # print("Best Energy " + str(best_E))
-        # print("Initial Solution " + str(best_S0))
-        # print("Optimal solution " + str(best_Sb))
         print("\n")
-    else:
-        #Execute all methods
-        print(f"Executing all methods")
-
-        #HillClimbing
-        if Me == 0:
-            t1 = time.time()
-            
-            eb_HC, Sb_HC, iters_HC = HC.HillClimbing(S0, args.iter_max, "flops")
-            
-            dt_HC = time.time()-t1
-            t1 = time.time()
-            
-            eb_SA, Sb_SA, iters_SA = SA.SimulatedAnnealing(S0, args.iter_max, 80, 0.8)
-            
-            dt_SA = time.time()-t1
-        time.sleep(1)
-
-        S0['simdType'] = args.simdType
-        #Parallel HillClimbing
-        t1 = time.time()
-        
-        best_E_PHC, best_S0_PHC, best_Sb_PHC = main_HC.execute(S0,args)
-        
-        dt_PHC = time.time()-t1
-        S0['simdType'] = args.simdType
-
-        #Greedy
-        t1 = time.time()
-        best_E_GR, best_S0_GR, best_Sb_GR = main_greedy.execute(S0, args)
-        dt_GR = time.time()-t1
-        S0['simdType'] = args.simdType
-
-        #Tabu Greedy
-        t1 = time.time()
-        best_E_TGR, best_S0_TGR, best_Sb_TGR = main_tabu_greedy.execute(S0, args)
-        dt_TGR = time.time()-t1
-        S0['simdType'] = args.simdType
-        if Me == 0:
-            print("\n")
-            print("========================= Best Parameters ======================")
-            print("Hill Climbing")
-            print("\n")
-            print("Best performance (Gflops) " + str(eb_HC))
-            print("Initial solution " + str(S0))
-            print("Optimal solution " + str(Sb_HC))
-            print("Execution time {:.3f}".format(dt_HC))
-
-            print("\n")
-            print("========================= Best Parameters ======================")
-            print("Parallel HillClimbing")
-            print("\n")
-            print("Best performance (Gflops) " + str(best_E_PHC))
-            print("Initial solution " + str(best_S0_PHC))
-            print("Optimal solution " + str(best_Sb_PHC))
-            print("Execution time {:.3f}".format(dt_PHC))
-            
-            print("\n")
-            print("========================= Best Parameters ======================")
-            print("Parallel Greedy")
-            print("\n")
-            print("Best performance (Gflops) " + str(best_E_GR))
-            print("Initial Solution " + str(best_S0_GR))
-            print("Optimal solution " + str(best_Sb_GR))
-            print("Execution time {:.3f}".format(dt_GR))
-            print("\n")
-
-            print("\n")
-            print("========================= Best Parameters ======================")
-            print("Parallel Tabu Greedy")
-            print("\n")
-            print("Best performance (Gflops) " + str(best_E_TGR))
-            print("Initial Solution " + str(best_S0_TGR))
-            print("Optimal solution " + str(best_Sb_TGR))
-            print("Execution time {:.3f}".format(dt_TGR))
-            print("\n")
-
-            print("\n")
-            print("========================= Best Parameters ======================")
-            print("Simulated Annealing")
-            print("\n")
-            print("Best performance (Gflops) " + str(eb_SA))
-            print("Initial Solution " + str(S0))
-            print("Optimal solution " + str(Sb_SA))
-            print("Execution time {:.3f}".format(dt_SA))
-            print("\n")
-            
     return best_E, best_Sb, best_S0, dt
 
 
