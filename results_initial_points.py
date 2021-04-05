b70b5e207f466fc618b5847ca02f68dfaa94e03e
MinhaTu <ailson.alexandre@hotmail.com>
2021-04-05 18:42:11 +0200

    implemented graph

--- results_initial_points.py
+++ results_initial_points.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 import numpy as np
+import pandas as pd
 import time
 import os
 import deploy_greedy_v3
@@ -14,6 +15,7 @@ import general_config as GC
 import simulated_annealing as SA
 import function_local_methods as run_LM
 from server_content.automated_compiling_tabu import define_copiler_settings
+import matplotlib.pyplot as plt
 
 
 class dotdict(dict):
@@ -23,21 +25,39 @@ class dotdict(dict):
     __delattr__ = dict.__delitem__
 
 args = {
-    'S0': [256, 256, 256, 4, 10,32,32,32],
-    'method': "HC",
-    'param_list': ['n1','n2','n3'], 
+    'S0': [512, 512, 512, 8, 10,32,32,32],
+    'method': "all",
+    'param_list': ['n1','n2','n3','tblock1','tblock2','tblock3'], 
     'iter_max': 10,
     'tabu_size': 8,
     'opt': 3,
     'simdType': "avx512",
-    'neighbourhood': "basic"
+    'neighbourhood': "other"
 }
 
 args = dotdict((args))
 
-best_E,best_Sb, best_S0,dt = run_LM.execute(args)
+imax = 1
+methods = ['HC', 'PHC', 'GR', 'TGR', 'SA']
+best_energies = dict()
+best_times = dict()
+for method in methods:
+    best_energies[method] = 0
+    best_times[method] = 0
 
-print(f'energy: {best_E}')
-print(f'S0 {best_S0}')
-print(f'best solution {best_Sb}')
+for i in range(0,imax):
+
+    for method in methods:
+        args.method = method
+        current_E,current_Sb, current_S0,current_dt = run_LM.execute(args)
+        
+        if current_E > best_energies[method]:
+            best_energies[method] = current_E
+            best_times[method] = current_dt
+
+if Me == 0:
+    df = pd.DataFrame({'Gflops': best_energies.values(), 'Execution time (s)': best_times.values()}, index = method)
+    ax = df.plot.bar(rot=0)
+    plt.show()
+
 
