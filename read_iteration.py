aa185acecdecf62bd4055e2243a8445e07cb3f18
charles217 <66557974+charles217@users.noreply.github.com>
2021-04-06 22:03:50 +0200

    Create read_iteration.py

---
+++ read_iteration.py
@@ -0,0 +1,6 @@
+data4 = pd.read_csv("Iteration.csv", index_col = 0)
+ax4 = data4.plot(rot=0, title = 'S0 = [256 256 256 32 10 32 32 32] équal, -np 4, ngbr = Others')
+ax4.legend(['Opt Speed (GFlops)', 'Opt Execution time (s)', 'Avg Speed (Gflops)', 'Avg Execution time (s)'])
+
+fig4 = ax4.get_figure()
+fig4.savefig('Iteration_figure.png')
