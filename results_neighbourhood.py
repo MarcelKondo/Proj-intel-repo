84c4d1d05936266a7e9eeccb56f10d271e171376
MinhaTu <ailson.alexandre@hotmail.com>
2021-04-06 19:18:02 +0200

    implemented results for neighbourhood

--- results_neighbourhood.py
+++ results_neighbourhood.py
@@ -31,7 +31,7 @@ class dotdict(dict):
 #Parameters to change
 
 args = {
-    'S0': [512, 512, 512, 32, 10,32,32,32],
+    'S0': [512, 512, 512, 8, 10,32,32,32],
     'method': "all",
     'param_list': ['n1','n2','n3','tblock1','tblock2','tblock3'], 
     'iter_max': 10,
