c0daa5d4a908d04133d3f214e2892d87aa500654
xamirb <80265845+xamirb@users.noreply.github.com>
2021-04-06 18:06:22 +0200

    Update general_config.py

--- general_config.py
+++ general_config.py
@@ -7,11 +7,11 @@ import random as rd
 from itertools import combinations
 
 param_space = {
-    'n1' : [160, 900, 16],
-    'n2' : [160, 900, 4],
-    'n3' : [160, 900, 4],
+    'n1' : [160, 512, 16],
+    'n2' : [160, 512, 4],
+    'n3' : [160, 512, 4],
     'nb_threads' : [32, 32, 0],
-    'nb_it' : [10, 100, 10],
+    'nb_it' : [10, 50, 10],
     'tblock1' : [16, 80, 16],
     'tblock2' : [16, 80, 4],
     'tblock3' : [16, 80, 4]
