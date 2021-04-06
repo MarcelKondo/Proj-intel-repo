ac89bd4b497973ea31a284977e4e6306bef68f9c
MinhaTu <ailson.alexandre@hotmail.com>
2021-04-06 09:40:43 +0200

    resuts parallel

--- results_initial_points.py
+++ results_initial_points.py
@@ -27,8 +27,11 @@ class dotdict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
+
+#Parameters to change
+
 args = {
-    'S0': [512, 512, 512, 32, 10,32,32,32],
+    'S0': [512, 512, 512, 8, 10,32,32,32],
     'method': "all",
     'param_list': ['n1','n2','n3','tblock1','tblock2','tblock3'], 
     'iter_max': 10,
@@ -41,14 +44,22 @@ args = {
 args = dotdict((args))
 
 define_copiler_settings(opLevel=args.opt, simdType=args.simdType, version="dev13")
-imax = 20
+
+
+
 methods = ['HC', 'PHC', 'GR', 'TGR', 'SA']
 best_energies = dict()
+average_energies = dict()
 best_times = dict()
+average_times = dict()
+
 for method in methods:
     best_energies[method] = 0
     best_times[method] = 0
+    average_energies[method] = 0
+    average_times[method] = 0
 
+imax = 2 # nb runs
 for i in range(0,imax):
 
     for method in methods:
@@ -56,6 +67,7 @@ for i in range(0,imax):
         if method == 'HC' or method == 'SA':
             if Me == 0:
                 current_E,current_Sb, current_S0,current_dt = run_LM.execute(args)
+
                 if current_E > best_energies[method]:
                     best_energies[method] = current_E
                     best_times[method] = current_dt
@@ -65,45 +77,29 @@ for i in range(0,imax):
                 if current_E > best_energies[method]:
                         best_energies[method] = current_E
                         best_times[method] = current_dt
-        if Me == 0:
-            print('\n')
-            print('best result so far: ')
-            print('\n')
-            print(f'best_energies: {best_energies}')
-            print(f'best_times: {best_times}')
-# if Me == 0:
-#     method = 'HC'
-#     args.method = method
-#     current_E,current_Sb, current_S0,current_dt = run_LM.execute(args)
-#     if current_E > best_energies[method]:
-#         best_energies[method] = current_E
-#         best_times[method] = current_dt
 
-# method = 'PHC'
-# args.method = method
-# current_E,current_Sb, current_S0,current_dt = run_LM.execute(args)
-# if Me == 0:
-#     if current_E > best_energies[method]:
-#         best_energies[method] = current_E
-#         best_times[method] = current_dt
-
-# method = 'GR'
-# args.method = method
-# current_E,current_Sb, current_S0,current_dt = run_LM.execute(args)
-# if Me == 0:
-#     if current_E > best_energies[method]:
-#         best_energies[method] = current_E
-#         best_times[method] = current_dt
+        average_energies[method] += current_E
+        average_times[method] += current_dt
+    if Me == 0:
+        print('\n')
+        print('best result so far: ')
+        print('\n')
+        print(f'best_energies: {best_energies}')
+        print(f'best_times: {best_times}')
 
 if Me == 0:
+    average_energies = {key:value/imax for key, value in average_energies.items()}
+    average_times = {key:value/imax for key, value in average_times.items()}
     print('\n')
     print('\n')
     print('\n')
     print(f'best_energies: {best_energies}')
     print(f'best_times: {best_times}')
-    df = pd.DataFrame({'Gflops': list(best_energies.values()), 'Execution time (s)': list(best_times.values())}, index = methods)
+    print(f'average_energies: {average_energies}')
+    print(f'average_times: {average_times}')
+    df = pd.DataFrame({'Gflops': list(best_energies.values()), 'Execution time (s)': list(best_times.values()), 'Average energy': list(average_energies.values()), 'Average time': list(average_times.values())}, index = methods)
     print(df)
     #ax = df.plot.bar(rot=0)
-    df.to_csv(r'~/Proj-intel-repo/InitialPoints.csv', index = True, header=True)
+    df.to_csv(r'~/Proj-intel-repo/InitialPointsParallel.csv', index = True, header=True)
 
 
