de83bb04690538449f5a82fd79c5f24793403838
charles217 <66557974+charles217@users.noreply.github.com>
2021-04-06 19:04:37 +0200

    Update result_iter.py

--- result_iter.py
+++ result_iter.py
@@ -72,8 +72,8 @@ for i in range(0,imax):
             if method == 'HC' or method == 'SA':
                 if Me == 0:
                     current_E,current_Sb, current_S0,current_dt = run_LM.execute(args)
-                    best_energies[method] = current_E
-                    best_times[method] = current_dt
+                    best_energies[method].append(current_E)
+                    best_times[method].append(current_dt)
             else:
                 current_E,current_Sb, current_S0,current_dt = run_LM.execute(args)
 
