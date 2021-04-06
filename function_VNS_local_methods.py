f138078389bccf70afc877f492e377cdeaf3b5d3
Benjamin-Wkd <63577432+Benjamin-Wkd@users.noreply.github.com>
2021-04-06 14:53:34 +0200

    Update function_VNS_local_methods.py

--- function_VNS_local_methods.py
+++ function_VNS_local_methods.py
@@ -37,17 +37,17 @@ param_space = {
     
  
 def execute(args):
-    S0 = {
-    'n1' : 256,
-    'n2' : 256,
-    'n3' : 256,
-    'nb_threads' : 32,
-    'nb_it' : 10,
-    'tblock1' : 32,
-    'tblock2' : 32,
-    'tblock3' : 32,
-    #'simdType' : 'avx512'
-    }
+  S0 = {
+  'n1' : 256,
+  'n2' : 256,
+  'n3' : 256,
+  'nb_threads' : 32,
+  'nb_it' : 10,
+  'tblock1' : 32,
+  'tblock2' : 32,
+  'tblock3' : 32,
+  #'simdType' : 'avx512'
+  }
 
   GC.define_neighbourhood(args.neighbourhood)  
     
