89382e39c135395042d0821a1ab48d01f8ac9160
xamirb <80265845+xamirb@users.noreply.github.com>
2021-04-06 14:41:38 +0200

    Update function_local_methods.py

--- function_local_methods.py
+++ function_local_methods.py
@@ -26,17 +26,6 @@ if Me == 0:
         
 
 def execute(args):
-    S0 = {
-    'n1' : 256,
-    'n2' : 256,
-    'n3' : 256,
-    'nb_threads' : 4,
-    'nb_it' : 10,
-    'tblock1' : 32,
-    'tblock2' : 32,
-    'tblock3' : 32,
-    #'simdType' : 'avx512'
-    }
     GC.define_usedParameters(args.param_list)
     GC.define_neighbourhood(args.neighbourhood)
 
